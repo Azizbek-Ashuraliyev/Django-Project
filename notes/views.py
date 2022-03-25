bc67a4954e00d49ba35cccd27800b03938128ad7
Azizbek Ashuraliev <ashuralievazizbek.de@gmail.com>
2022-03-25 18:46:39 +0500

    Building a Robust Front End in Django

--- notes/views.py
+++ notes/views.py
@@ -1,13 +1,18 @@
 from ast import Not
+from dataclasses import field
 from pyexpat import model
 from statistics import mode
 from unittest import mock
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Notes
 
+class NotesCreateView(CreateView):
+    model = Notes
+    fields = ['title','text']
+    success_url = '/smart/notes'
 
 class NotesListView(ListView):
     template_name = 'notes/notes_list.html'
